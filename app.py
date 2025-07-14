bc334cca151ac240e15c0977e5f279e373a2a9b2
taysk888 <102202742+taysk888@users.noreply.github.com>
2025-07-14 04:03:50 +0000

    Done

--- app.py
+++ app.py
@@ -9,15 +9,17 @@ def index():
     return(render_template("index.html"))
 
 @app.route("/prediction",methods=["GET","POST"])
+
 def prediction():
     q = float(request.form.get("q"))
 
-    #load model
+    # load model
     model = joblib.load("dbs.jl")
 
+    # make prediction
     pred = model.predict([[q]])
 
-    return(render_template("prediction.html",r=pred)
+    return(render_template("prediction.html",r=pred))
 
 if __name__ == "__main__":
     app.run()
