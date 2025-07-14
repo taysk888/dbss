a40ea31aabd6ee0a0805f60c3dc1c85d57ddbd60
taysk888 <102202742+taysk888@users.noreply.github.com>
2025-07-14 13:48:28 +0000

    Done

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 
 from flask import Flask, render_template, request
 import joblib
+from groq import Groq
+
+import os
+
+
+# for cloud ..........
 
 app = Flask(__name__)
 
@@ -8,17 +14,43 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-@app.route("/prediction",methods=["GET","POST"])
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    # db
+    return(render_template("main.html"))
 
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+    # load model
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llama_reply.html",r=completion.choices[0].message.content))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
-
     # load model
     model = joblib.load("dbs.jl")
-
     # make prediction
     pred = model.predict([[q]])
-
     return(render_template("prediction.html",r=pred))
 
 if __name__ == "__main__":
